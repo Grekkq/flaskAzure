25fd0a6f518d880cd069eb44730e9ca0b0e7abe5
Grekkq <grekkq97@gmail.com>
2022-01-29 00:07:47 +0100

    Database connection

--- app.py
+++ app.py
@@ -1,25 +1,68 @@
+import config
 from flask import Flask
 from pathlib import Path
+import azure.cosmos.documents as documents
+import azure.cosmos.cosmos_client as cosmos_client
+
+HOST = config.settings["host"]
+MASTER_KEY = config.settings["master_key"]
+DATABASE_ID = config.settings["database_id"]
+CONTAINER_ID = config.settings["container_id"]
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY="dev",
         # DATABASE=Path.joinpath(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
-    @app.route('/')
-    def hello():
-        return 'Pustka!'
+    def create_items(container):
+        print("\nCreating item\n")
+        link_item = {
+            "id": 1,
+            "name": "ciekawylink1",
+            "link": "google.com",
+        }
+        container.create_item(body={link_item})
 
-    @app.route('/hello')
+    def read_items(container):
+        print('\nReading all items in a container\n')
+
+        # NOTE: Use MaxItemCount on Options to control how many items come back per trip to the server
+        #       Important to handle throttles whenever you are doing operations such as this that might
+        #       result in a 429 (throttled request)
+        item_list = list(container.read_all_items(max_item_count=10))
+
+        print('Found {0} items'.format(item_list.__len__()))
+
+        for doc in item_list:
+            print('Item Id: {0}'.format(doc.get('id')))
+
+        return item_list
+
+    @app.route("/")
+    def home():
+        client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
+        db = client.get_database_client(DATABASE_ID)
+        container = db.get_container_client(CONTAINER_ID)
+        return read_items(container)
+
+    @app.route("/hello")
     def hello():
-        return 'No hej!'
+        return "No hej!"
+
+    @app.route("/createItem")
+    def create_item():
+        client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
+        db = client.get_database_client(DATABASE_ID)
+        container = db.get_container_client(CONTAINER_ID)
+        create_items(container)
+        return "Created!"
 
     return app
