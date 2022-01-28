7942593288ced5f1f2540ada32778b3e52f0e866
Grekkq <grekkq97@gmail.com>
2022-01-28 23:21:11 +0100

    Example copypaste

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask
+from pathlib import Path
 
-app = Flask(__name__)
 
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        # DATABASE=Path.joinpath(app.instance_path, 'flaskr.sqlite'),
+    )
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.route('/')
+    def hello():
+        return 'Pustka!'
+
+    @app.route('/hello')
+    def hello():
+        return 'No hej!'
+
+    return app
